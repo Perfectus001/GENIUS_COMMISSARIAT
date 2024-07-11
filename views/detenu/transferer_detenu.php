8d301f5883f5acba03685ee2a2906b8cbbcaba2a
Perfectus001 <jeanmikely001@gmail.com>
2024-07-11 15:40:22 -0400

    MJ -V:17 commit

--- views/detenu/transferer_detenu.php
+++ views/detenu/transferer_detenu.php
@@ -8,27 +8,41 @@
 
     <!-- Bootstrap CSS -->
     <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
+    <link href="../../partials/assets/img/favicon.png" rel="icon">
+    <link href="../../partials/assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+    <!-- Google Fonts -->
+    <link href="https://fonts.gstatic.com" rel="preconnect">
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Nunito:300,300i,400,400i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
 
+    <!-- Vendor CSS Files -->
+    <link href="../../partials/assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href="../../partials/assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+    <link href="../../partials/assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+    <link href="../../partials/assets/vendor/quill/quill.snow.css" rel="stylesheet">
+    <link href="../../partials/assets/vendor/quill/quill.bubble.css" rel="stylesheet">
+    <link href="../../partials/assets/vendor/remixicon/remixicon.css" rel="stylesheet">
+    <link href="../../partials/assets/vendor/simple-datatables/style.css" rel="stylesheet">
+
+    <link rel="stylesheet" href="./cssDetenu/style.css">
+
+    <!-- Template Main CSS File -->
+    <link href="../../partials/assets/css/style.css" rel="stylesheet">
     <style>
         body {
             padding: 20px;
         }
 
         .sidebar {
-            width: 300px;
-            height: 614px;
-            background-color: #f0f0f0;
-            padding: 20px;
-            float: left;
             margin-right: 20px;
         }
 
         .content {
-            margin-left: 340px; /* 300px sidebar width + 20px margin */
+            margin-left: 0px; /* 300px sidebar width + 20px margin */
         }
 
         .form-container {
-            margin-top: 20px; /* Adjust as needed */
+            margin-top: 50px; /* Adjust as needed */
+            width: 250%;
         }
     </style>
 </head>
@@ -38,7 +52,7 @@
         <?php include('../../partials/sidebar.php'); ?>
     </div>
 
-    <div class="content">
+    <div class="content ">
         <?php
         if (isset($_GET['detenu'])) {
             $detenu = $_GET['detenu'];
@@ -77,6 +91,19 @@
 
     <!-- Bootstrap JS -->
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+
+    
+    <script src="../../partials/assets/vendor/apexcharts/apexcharts.min.js"></script>
+    <script src="../../partials/assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script src="../../partials/assets/vendor/chart.js/chart.umd.js"></script>
+    <script src="../../partials/assets/vendor/echarts/echarts.min.js"></script>
+    <script src="../../partials/assets/vendor/quill/quill.js"></script>
+    <script src="../../partials/assets/vendor/simple-datatables/simple-datatables.js"></script>
+    <script src="../../partials/assets/vendor/tinymce/tinymce.min.js"></script>
+    <script src="../../partials/assets/vendor/php-email-form/validate.js"></script>
+
+    <!-- Template Main JS File -->
+    <script src="../../partials/assets/js/main.js"></script>
 </body>
 
 </html>
