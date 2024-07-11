196b4d091cfe59716e541e58abe6857bba41a847
ingivenson <tiluipetithomme1@gmail.com>
2024-07-11 13:41:52 -0400

    Ivenson 2.7

--- controllers/DetenuController.php
+++ controllers/DetenuController.php
@@ -135,6 +135,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 
                 if($detenuDao->save($detenu)){
                     echo "<p>Enregistrement effectue avec Succes</p>";
+                    header('Location: ../Views/detenu/afficher_detenu.php');
                 }else{
                     echo "Une erreur s'est produite";
                 }
@@ -265,6 +266,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 
                 if($detenuDao->update($detenu)){
                     echo "<p>Modification effectuee avec Succes</p>";
+                    header('Location: ../Views/detenu/afficher_detenu.php');
                 }else{
                     echo "Une erreur s'est produite";
                 }
