9ca05279bbaf2cae36eda1532f773d04ca9a5318
Perfectus001 <jeanmikely001@gmail.com>
2024-07-11 15:32:15 -0400

    MJ -V:16 commit

--- views/detenu/transferer_detenu.php
+++ views/detenu/transferer_detenu.php
@@ -5,34 +5,78 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+
+    <!-- Bootstrap CSS -->
+    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
+
+    <style>
+        body {
+            padding: 20px;
+        }
+
+        .sidebar {
+            width: 300px;
+            height: 614px;
+            background-color: #f0f0f0;
+            padding: 20px;
+            float: left;
+            margin-right: 20px;
+        }
+
+        .content {
+            margin-left: 340px; /* 300px sidebar width + 20px margin */
+        }
+
+        .form-container {
+            margin-top: 20px; /* Adjust as needed */
+        }
+    </style>
 </head>
 
 <body>
-    <?php
-    if (isset($_GET['detenu'])) {
-        $detenu = $_GET['detenu'];
-    }
-    ?>
+    <div class="sidebar">
+        <?php include('../../partials/sidebar.php'); ?>
+    </div>
 
-    <?php if (isset($detenu)) { ?>
-        <form method="post" action="../../controllers/detenuController.php">
-            <input type="hidden" name="idDetenu" value="<?= $detenu['idDetenu'] ?>">
-            <div>
-                <label for="codePrison">code prison</label>
-                <input type="text" name="codePrison" id="codePrison" value="<?php echo isset($detenu['codePrison']) ? htmlspecialchars($detenu['codePrison']) : ''; ?>">
+    <div class="content">
+        <?php
+        if (isset($_GET['detenu'])) {
+            $detenu = $_GET['detenu'];
+        }
+        ?>
+
+        <?php if (isset($detenu)) { ?>
+            <div class="form-container">
+                <form method="post" action="../../controllers/detenuController.php">
+                    <input type="hidden" name="idDetenu" value="<?= $detenu['idDetenu'] ?>">
+                    <div class="form-group">
+                        <label for="codePrison">Code Prison</label>
+                        <input type="text" class="form-control" id="codePrison" name="codePrison" value="<?php echo isset($detenu['codePrison']) ? htmlspecialchars($detenu['codePrison']) : ''; ?>">
+                    </div>
+                    <div class="form-group">
+                        <input type="submit" class="btn btn-primary" value="Transferer" name="sub">
+                    </div>
+                </form>
             </div>
-            <div><input type="submit" value="Transferer" name="sub"></div>
-        </form>
-    <?php } else { ?>
-        <form method="post" action="../../controllers/detenuController.php">
-            <input type="hidden" name="idDetenu" value="<?php echo isset($_GET['idDetenu']) ? htmlspecialchars($_GET['idDetenu']) : ''; ?>">
-            <div>
-                <label for="codePrison">CODE PRISON</label>
-                <input type="text" name="codePrison" id="codePrison" value="<?php echo isset($_GET['codePrison']) ? htmlspecialchars($_GET['codePrison']) : ''; ?>">
-                <br><?php if (isset($_GET['error_codePrison'])) echo "<p style='color: red;'>" . $_GET['error_codePrison'] . "</p>"; ?>
+        <?php } else { ?>
+            <div class="form-container">
+                <form method="post" action="../../controllers/detenuController.php">
+                    <input type="hidden" name="idDetenu" value="<?php echo isset($_GET['idDetenu']) ? htmlspecialchars($_GET['idDetenu']) : ''; ?>">
+                    <div class="form-group">
+                        <label for="codePrison">Code Prison</label>
+                        <input type="text" class="form-control" id="codePrison" name="codePrison" value="<?php echo isset($_GET['codePrison']) ? htmlspecialchars($_GET['codePrison']) : ''; ?>">
+                        <?php if (isset($_GET['error_codePrison'])) echo "<p style='color: red;'>" . $_GET['error_codePrison'] . "</p>"; ?>
+                    </div>
+                    <div class="form-group">
+                        <input type="submit" class="btn btn-primary" value="Transferer" name="sub">
+                    </div>
+                </form>
             </div>
-            <div><input type="submit" value="Transferer" name="sub"></div>
-        </form>
-    <?php } ?>
+        <?php } ?>
+    </div>
+
+    <!-- Bootstrap JS -->
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 </body>
+
 </html>
